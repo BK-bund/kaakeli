6cb93cb3c6bf886559e9e08b1b33a74214792cea
Ben Schumacher <ben.schumacher@cjf-berlin.de>
2017-03-16 02:07:38 +0100

    Fixed placeholder text

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
 <?php get_template_part('navi' , ''); ?>
 
 <div id="body">
-  I am Index
   <h3><?php bloginfo('name'); ?></h3>
   <h2 class="seitentitel"><?php the_title(); ?></h2>
   <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
@@ -11,7 +10,7 @@
             <p>erstellt am: <?php the_date('d.m.Y'); ?> | von: <?php the_author(); ?>
           </div>
           <div class="entry">
-      <?php the_content(); ?>
+      	    <?php the_content(); ?>
           </div>
   <?php endwhile; ?> <?php endif; ?>
 </div><!-- body -->
